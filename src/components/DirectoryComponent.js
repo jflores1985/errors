3270c8f16325c4f1476882ab2c4b707a2332d3b3
Jesus Flores <misterj1985@gmail.com>
2020-10-24 12:44:27 -0500

    Week 3 Assisgnment

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 function RenderDirectoryItem({campsite}) {
@@ -28,12 +28,17 @@ function Directory(props) {
     return (
         <div className="container">
             <div className="row">
-                {directory}
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
             </div>
             <div className="row">
-                <div>
-
-                </div>
+                {directory}
             </div>
         </div>
     );
